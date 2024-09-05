045e5b915d571855245ea8ea396f97c607035f85
Miriã Ribeiro <rs654680@gmail.com>
2024-09-05 20:54:53 -0300

    add js
    add função de javascript

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("DIO (Digital Innovation One)");
+//para mostrar apenas uma variavel, tipo o titulo = .titulo
+//para voce comentar uma variavel no arquivo para html usa ${dados[0].titulo}
+
+function pesquisar() {
+    //console.log(dados);
+    //document.getElementById = dentro do arquivo html buscar pelo id ("")
+    // Busca a seção onde os resultados serão exibidos.
+    // O elemento com o ID "resultados-pesquisa" será o container para os resultados.
+    let section = document.getElementById("resultados-pesquisa")
+    // Inicializa uma string vazia para armazenar os resultados formatados em HTML.
+    let resultados = ""
+    // Itera sobre cada item (dado) da lista de dados (provavelmente um array).
+    // Para cada dado, cria um novo elemento HTML com as informações do resultado.
+    //para cada dado dentro da lista de dados.
+    for (let dado of dados) {
+        // Constrói uma string de HTML para cada resultado, incluindo:
+        // - Um título (h2) com um link para mais informações.
+        // - Uma descrição (p) com a classe "descricao-meta".
+        // - Um link para mais informações.
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank"> ${dado.titulo} </a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao} </p>
+            <a href=${dado.link}  target="_blank">Mais informações</a>
+        </div> 
+        `
+    }
+    // Atribui a string com todos os resultados ao conteúdo HTML da seção.
+    // Isso substitui o conteúdo anterior da seção pelos novos resultados.
+    section.innerHTML = resultados
+}
+
+
+
+
+
+
+
+    
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+
+//para mostrar apenas uma variavel usa o ponto e o nome da variavel. ex titulo = .titulo
+let dados = [
+    {
+        titulo : "DIO (Digital Innovation One)",
+        descricao : "A DIO oferece uma ampla variedade de cursos nas áreas de tecnologia, dados, negócios e muito mais. A Digital Innovation One (DIO) disponibiliza cursos on-line gratuitos sobre 8 linguagens de programação1. Os cursos são voltados tanto para profissionais que estão no começo da carreira como para aqueles que já estão na área e buscam aprimorar suas habilidades1. A DIO oferece 185 cursos gratuitos em diversas áreas de desenvolvimento de software, incluindo .NET, Android, Angular, Arquitetura de sistemas, AWS, Azure, Banco de dados, Docker, entre outros",
+        link : "https://www.dio.me/"
+    },
+    {
+        titulo : "Google",
+        descricao : "O Google oferece diversos cursos gratuitos em sua plataforma de aprendizado, cobrindo desde fundamentos de programação até tópicos mais avançados de inteligência artificial.",
+        link : "https://grow.google/intl/pt/courses-and-tools/"
+    },
+    {
+        titulo : "Alura",
+        descricao : "Alura: Um Oceano de Conhecimento Gratuito à Sua Disposição! A Alura, uma das plataformas de ensino online mais populares do Brasil, oferece uma vasta biblioteca de cursos gratuitos em diversas áreas da tecnologia. Se você busca aprimorar suas habilidades e se destacar no mercado de trabalho, a Alura é o lugar ideal para começar",
+        link : " https://www.alura.com.br/"
+    }
+];
+
+
